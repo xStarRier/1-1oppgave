863485a6ddf9e48560720391df0d08aa003ec573
xStarRier <stephanniequynh@gmail.com>
2024-11-28 14:50:34 +0100

    Add project files.

---
+++ 1-1oppgave/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1oppgave //info på personene, alder, epost, 
+{
+    internal class Person
+    {
+        // Navn
+        private string _name;
+        // Alder
+        // Epost
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public void AddPeople()
+        {
+
+        }
+        // Person.AddPeople()
+    }
+}
+}
---
+++ 1-1oppgave/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Lag en applikasjon hvor vi lager noen personer med noen attributter, deretter lag en meny hvor man kan 1) se på personer 2) avslutte program.
+using System.ComponentModel.Design;
+
+string[] dataBase = { "Martin", "Rebecka", "Marie", "Ellie" };
+List<string> People = new List<string> { "Martin" , "Rebecka", "Marie", "Ellie" };
+People.Add("Terje");
+//var newPerson = new Person("Kompis");
+
+while(true){
+    Meny();
+    InputFunc();
+}
+
+void InputFunc(){
+    var userInput = Console.ReadLine();
+
+    // hvis input == 1 -> Vise personer (egen funksjon?)
+    if(userInput == "1")
+    {
+        ShowPeople();
+    }
+    else if (userInput == "2")
+    {
+        AddPeople(); //hvis samme navn i
+    }
+    else
+    {
+        Exit();
+    }
+
+    // hvis input == 2 -> avslutte
+}   
+
+void Meny()
+{   
+    Console.WriteLine("Welkommen Til The Get Database!");
+    Console.WriteLine("1) Se personer");
+    Console.WriteLine("2) Legg til person");
+    Console.WriteLine("3) Avslutt personer");
+    
+}
+
+void ShowPeople()
+{
+    for (var i = 0; i < People.Count; i++)  // skriver ut alt i dataBase
+    {
+        Console.WriteLine(People[i]);
+    }
+}
+
+void AddPeople()
+{
+    People.Add(Console.ReadLine());
+}
+void Exit()
+{
+    Environment.Exit(0);
+}
+
+// Welkommen TIl The Get Database!
+// Valg: 
+// 1) Se personer
+// 2) Avslutt program
+
+// Martin
+// Rebecka
+// ...
+
+// Welkommen TIl The Get Database!
+// Valg: 
+// 1) Se personer
+// 2) Avslutt program
